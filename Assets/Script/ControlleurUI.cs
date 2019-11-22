218c53ade5722fe776356155c02f8c4ec004ec91
albert ozouaki <ozouakimanon@yahoo.fr>
2019-11-22 17:51:04 +0100

    script controller added ui

---
+++ Assets/Script/ControlleurUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlleurUI : MonoBehaviour
+{
+    [SerializeField] private Button Build_toggle_btn;
+    [SerializeField] private Button Scan_toggle_btn;
+    [SerializeField] private Button Library_btn;
+    [SerializeField] private Button Scan_btn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
